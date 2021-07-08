7870a3a896d05b881406d445b2c316308bb83627
Jesus Alberto del Moral Cupil <idv18c.jmoral@uartesdigitales.edu.mx>
2021-07-08 14:58:15 -0500

    CU-xtg2vb[IN PROGRESS]
    > Temporizadores para el fuego
    > Implementacion para destruir el fuego y la bomba despues del tiempo

--- Assets/Scripts/ucBombs.cs
+++ Assets/Scripts/ucBombs.cs
@@ -6,12 +6,16 @@ public
 class ucBombs : MonoBehaviour {
 
   float m_timer = 3.5f; //Time in seconds
+  float m_timerFire = 3.5f; //Time in seconds
   float m_actualTime; 
-  int m_range = 1; //Range in tiles
+  float m_actualTimeFire; 
+  int m_range = 2; //Range in tiles
   int m_tileSize;
   public GameObject m_thisBomb;
   public GameObject m_firePrefab;
   public bool m_isActive = false;
+  List<GameObject> fire = new List<GameObject>();
+  bool m_isExploded = false;
 
 
   // Start is called before the first frame update
@@ -27,11 +31,17 @@ class ucBombs : MonoBehaviour {
 
     if (m_isActive) {
       m_actualTime -= Time.deltaTime;
+      m_actualTimeFire -= Time.deltaTime;
     }
 
-    if (0 >= m_actualTime) {
+    if (0 >= m_actualTime && !m_isExploded) {
       Explode();
     }
+    
+    if (0 >= m_actualTimeFire && m_isExploded) {
+      FireDespawn();
+    }
+    
   }
 
   public void
@@ -43,29 +53,50 @@ class ucBombs : MonoBehaviour {
   void
   Explode() {
     m_actualTime = m_timer;
-    m_thisBomb.SetActive(false);
     //Generate the explotion and detect the player
     //Directions
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i <= 3; ++i) {
       //Range
-      Vector3 tmpPos = m_thisBomb.gameObject.transform.position;
-      switch (i) {
-        case 0: //derecha
-          tmpPos.x += m_thisBomb.GetComponent<Collider>().bounds.size.x;
-          break;
-        case 1: //abajo
-          tmpPos.z -= m_thisBomb.GetComponent<Collider>().bounds.size.z;
-          break;
-        case 2: //izquierda
-          tmpPos.x -= m_thisBomb.GetComponent<Collider>().bounds.size.x;
-          break;
-        case 3: //ariba
-          tmpPos.z += m_thisBomb.GetComponent<Collider>().bounds.size.z;
-          break;
-      }
-      for (int j = 0; j < m_range; ++j) { 
-        GameObject tmpBomb = Instantiate(m_firePrefab, tmpPos, Quaternion.identity);
+      for (int j = 1; j <= m_range; ++j) {
+        Vector3 tmpPos = m_thisBomb.gameObject.transform.position;
+        switch (i) {
+          case 0: //derecha
+            tmpPos.x += (m_thisBomb.GetComponent<BoxCollider>().size.x * j);
+
+            break;
+          case 1: //abajo
+            tmpPos.z -= (m_thisBomb.GetComponent<BoxCollider>().size.z * j);
+
+            break;
+          case 2: //izquierda
+            tmpPos.x -= (m_thisBomb.GetComponent<BoxCollider>().size.x * j);
+
+            break;
+          case 3: //ariba
+            tmpPos.z += (m_thisBomb.GetComponent<BoxCollider>().size.z * j);
+            break;
+        }
+
+        GameObject tmpFire = Instantiate(m_firePrefab, tmpPos, Quaternion.identity);
+        fire.Add(tmpFire);
       }
     }
+    m_isExploded = true;
+    m_actualTimeFire = m_timerFire;
   }
+
+
+  void FireDespawn() {
+
+
+    for (int i = 0; i < fire.Count; ++i ) {
+      DestroyObject(fire[i]);
+    }
+
+
+    m_thisBomb.SetActive(false);
+    fire = new List<GameObject>();
+    DestroyObject(m_thisBomb);
+  }
+
 }
